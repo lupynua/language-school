385c244d8a85c4cb061f153687ef3befbcd93294
Fleeck <zoorned@gmail.com>
2015-07-31 11:37:39 +0300

    LVUIDF-24:Deleted unneccesary code.

--- app/assets/javascripts/collections/pages.js
+++ app/assets/javascripts/collections/pages.js
@@ -1,8 +1,4 @@
 Bb.Collections.Pages = Backbone.Collection.extend({
   url: "/api/v1/pages",
-  model: Bb.Models.Page,
-
-  initialize: function(props) {
-    if (props && props.path) this.url += props.path;
-  }
+  model: Bb.Models.Page
 });
