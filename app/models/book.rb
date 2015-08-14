26e5627864a60ade4b6c0d3c4217a4a7291345b9
Marta Stefanyshyn <martaolehivna@gmail.com>
2015-08-14 07:25:15 -0700

    Rspec with mock_model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ActiveRecord::Base
 	mount_uploader :attachment, AttachmentUploader 
     validates :title, :description, presence: true, length: { minimum: 3 }
-    #validates :attachment, uniqueness: { scope: :book_id }
+    validates :attachment, presence: true
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,33 +1,28 @@
 require 'rails_helper'
-describe Book, :type => :model  do
-  it "is valid with a title, description" do
-    book = Book.new(
-      title: 'Grammar',
-      description: 'consist of all rules')
-    expect(book).to be_valid
-  end
 
-  it "has a valid factory" do
+describe Book, :type => :model  do
+  it "should create valid book" do
     expect(FactoryGirl.create(:book)).to be_valid
   end
+  
   it "is invalid without a title" do
     expect(FactoryGirl.build(:book, title: nil)).to_not be_valid
   end
+  
   it "is invalid without a description" do
     expect(FactoryGirl.build(:book, description: nil)).to_not be_valid
   end
-   it "booklist is empty" do
+  
+  it "booklist is empty" do
     expect(Book.count).to eq 0
-   end
-   it "returns check title of the book" do
-     book = Book.new(title: 'Book', description: 'About Book',
-     attachment: 'lalala')
-     expect(book.title).to eq 'Book'
-   end
-   it "returns check description of the book" do
-     book = Book.new(title: 'Second', description: 'About second Book',
-     attachment: 'lalala')
-     expect(book.description).to eq 'About second Book'
-   end
   end
+end
+
+describe "attachment checks" do
+  let(:book) {  mock_model(Book, attachment: nil, presense: false) }
+
+  it "should save with attachment" do
+    expect(book).to be_valid
+  end
+end
   
